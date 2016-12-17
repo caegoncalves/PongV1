61849c1247563d137850dd5fb5dddb4b1894595f
Cae Goncalves <hello@cae.works>
2016-12-17 17:56:23 -0200

    Projeto PONG com GameActivity criada e limpa

---
+++ app/src/main/java/works/cae/pongv1/GameActivity.java
@@ -0,0 +1,19 @@
+package works.cae.pongv1;
+
+import android.os.Bundle;
+
+import works.cae.simplegameenginev1.SGActivity;
+
+/**
+ * Created by salfl on 17/12/16.
+ */
+
+public class GameActivity extends SGActivity {
+    public static final String TAG = "PongV1";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+}
