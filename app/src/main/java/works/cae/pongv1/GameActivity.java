75479a8df89607c75bab6073c9f204f7e7ea3d62
Cae Goncalves <hello@cae.works>
2017-01-15 13:55:24 -0200

    Versão inicial corrigida e finalizada, limpa para receber activity

--- app/src/main/java/works/cae/pongv1/GameActivity.java
+++ app/src/main/java/works/cae/pongv1/GameActivity.java
@@ -1,8 +1,10 @@
 package works.cae.pongv1;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import works.cae.simplegameenginev1.SGActivity;
+import works.cae.simplegameenginev1.SGPreferences;
 
 /**
  * Created by salfl on 17/12/16.
@@ -14,6 +16,31 @@ public class GameActivity extends SGActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        enableFullScreen();
+        enableKeepScreenOn();
+
+        SGPreferences preferences = new SGPreferences(this);
+        if(preferences.getInt("first_time", -1) == -1) {
+            preferences.begin()
+                    .putInt("first_time", 1)
+                    .putInt("difficulty", 0)
+                    .putInt("high_score", 15)
+                    .end();
+
+            Log.d(TAG, "Primeira inicialização");
+        }
+
+        StringBuilder stringBuilder = new StringBuilder();
+
+        stringBuilder.append("Nível de dificuldade: ");
+        stringBuilder.append(preferences.getInt("difficulty", 0));
+        Log.d(TAG, stringBuilder.toString());
+
+        stringBuilder.setLength(0);
+        stringBuilder.append("High score: ");
+        stringBuilder.append(preferences.getInt("high_score", 0));
+        Log.d(TAG, stringBuilder.toString());
     }
 
 }
