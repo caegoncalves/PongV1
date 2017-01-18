91be3f2374eb12a4d51e7d85799532480f82a095
Cae Goncalves <hello@cae.works>
2017-01-17 23:27:37 -0200

    Alterações na GameView para adicionar movimento ao paddle oponente e bola com os métodos correspondentes

--- app/src/main/java/works/cae/pongv1/GameView.java
+++ app/src/main/java/works/cae/pongv1/GameView.java
@@ -15,16 +15,22 @@ import works.cae.simplegameenginev1.SGView;
 
 public class GameView extends SGView {
 
-    private final static int BALL_SIZE = 16;
+    private final static int BALL_SIZE = 16; // Tamanho da bola
     private final static int DISTANCE_FROM_EDGE = 16;
     private final static int PADDLE_HEIGHT = 98;
     private final static int PADDLE_WIDTH = 23;
+
+    private final static int BALL_SPEED = 2; // Velocidade da bola
+    private final static int OPPONENT_SPEED = 2; // Velocidade do paddle oponente
 
     private Rect mBallDestination = new Rect();
     private Rect mOpponentDestination = new Rect();
     private Rect mPlayerDestination = new Rect();
 
     private Paint mTempPaint = new Paint();
+
+    private boolean mBallMoveRight = true;
+    private boolean mOpponentMoveDown = true;
 
     @Override
     public void setup() {
@@ -51,12 +57,67 @@ public class GameView extends SGView {
     }
 
     @Override
-    public void step(Canvas canvas) {
+    public void step(Canvas canvas) { // Sobrepondo elementos do jogo à tela
+        moveBall();
+        moveOpponent();
+
         mTempPaint.setColor(Color.RED);
 
         canvas.drawRect(mPlayerDestination, mTempPaint);
         canvas.drawRect(mBallDestination, mTempPaint);
         canvas.drawRect(mOpponentDestination, mTempPaint);
+    }
+
+    private void moveOpponent() {
+        Point viewDimensions = getDimensions();
+
+        if(mOpponentMoveDown) { // Testa se a direção do oponente é para BAIXO
+            mOpponentDestination.top += OPPONENT_SPEED; // Desenha o topo do paddle oponente 2px para BAIXO
+            mOpponentDestination.bottom += OPPONENT_SPEED; // Desenha a base do paddle oponente 2px para BAIXO
+
+            if(mOpponentDestination.bottom >= viewDimensions.y) { // Testa se a base ultrapassou o canto INFERIOR da tela (efeito de encostar/rebater)
+                mOpponentDestination.top = viewDimensions.y - PADDLE_HEIGHT; // Recua o topo em 2px para BAIXO usando a medida do paddle
+                mOpponentDestination.bottom = viewDimensions.y; // Recoloca a base 2px para BAIXO (já que ela teria passado a extremidade em 2px que é a velocidade)
+
+                mOpponentMoveDown = false; // Muda para false porque o movimento da bola agora será para CIMA
+            }
+        } else {
+            mOpponentDestination.top -= OPPONENT_SPEED; // Desenha o topo do paddle oponente 2px para CIMA
+            mOpponentDestination.bottom -= OPPONENT_SPEED;// Desenha a base do paddle oponente 2px para CIMA
+
+            if(mOpponentDestination.top < 0) {
+                mOpponentDestination.top = 0;
+                mOpponentDestination.bottom = PADDLE_HEIGHT;
+
+                mOpponentMoveDown = true; // Muda para false porque o movimento da bola agora será para BAIXO
+            }
+        }
+    }
+
+    private void moveBall() {
+        Point viewDimensions = getDimensions();
+
+        if(mBallMoveRight) { // Testa se a direção é DIREITA
+            mBallDestination.left += BALL_SPEED; // Desenha a aresta ESQUERDA da bola 2 px para DIREITA
+            mBallDestination.right += BALL_SPEED; // Desenha a aresta DIREITA da bola 2 px para DIREITA
+
+            if(mBallDestination.right >= viewDimensions.x) { // Testa se a aresta DIREITA ultrapassou o canto DIREITO da tela (efeito de encostar/rebater)
+                mBallDestination.left = viewDimensions.x - BALL_SIZE; // Recua a aresta ESQUERDA em 2px para a ESDQUERDA
+                mBallDestination.right = viewDimensions.x; // Recoloca a aresta DIREITA 2px para a ESQUERDA (já que ela teria passado a extremidade em 2px que é a velocidade)
+
+                mBallMoveRight = false; // Muda para false porque o movimento da bola agora é para a ESQUERDA
+            }
+        } else { // Lógica inversa para movimentar a bola para a ESQUERDA
+            mBallDestination.left -= BALL_SPEED;
+            mBallDestination.right -= BALL_SPEED;
+
+            if(mBallDestination.left <= 0) {
+                mBallDestination.left = 0;
+                mBallDestination.right =  BALL_SIZE; // Aqui recebe o tamanho da bola pra manter o tamanho do desenho
+
+                mBallMoveRight = true;
+            }
+        } // Fim do moveBall()
     }
 
     public GameView(Context context) {
